7eb7d28d1479b22b5f0ceb9e9170506319693a8e
William-Hon <32whon@gmail.com>
2025-12-30 14:34:29 -0500

    images names fixed

--- src/sections/GetInvolvedSection.jsx
+++ src/sections/GetInvolvedSection.jsx
@@ -9,7 +9,7 @@ const GetInvolvedSection = () => {
         </h1>
         <GetInvolvedPanel
           color="red"
-          photoURL="/assets/images/donor.png"
+          photoURL="/assets/images/nusis-workshop.png"
           text="Looking to support NUImpact? Find out what a donation to our fund can do." />
       </div>
 
